16aa19c1bce2a18f27685e066a657db4ff6e0f90
Dixita33 <168657070+Dixita33@users.noreply.github.com>
2024-05-05 21:47:47 +0530

    Create Pract6.cpp

---
+++ Pract6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=40,c; 
+c=a;
+  cout<<"\nc="<<c;
+c+=a;
+  cout<<"\nc="<<c;
+c-=a;
+  cout<<"\nc="<<c;
+c*=a;
+  cout<<"\nc="<<c;
+c/=a;
+  cout<<"\nc="<<c;
+c%=a;
+  cout<<"\nc="<<c;
+return 0;
+}
+
+
