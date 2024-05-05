146579ff88618d5d5900b1da2115d56418aab1ba
Dixita33 <168657070+Dixita33@users.noreply.github.com>
2024-05-05 21:53:16 +0530

    Create Pract7.cpp

---
+++ Pract7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10,b=5;
+cout<<"\nAnd:"<<a&b;
+cout<<"\nOr:"<<a|b;
+cout<<"\nShift right:"<<a<<1;
+cout<<"\nLeft right:"<<a>>1;
+cout<<"\nNot:"<<~a;
+cout<<"\nXor:"<<a^b;
+
+return 0;
+}
