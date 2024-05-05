536cb229e087f4e533df5906e3550854177504c7
Dixita33 <168657070+Dixita33@users.noreply.github.com>
2024-05-05 20:49:39 +0530

    Update and rename Pract2.main to Pract1.cpp

---
+++ Pract1.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    int roll_num;
+    char name,dept;
+    cout<<"roll_num";
+    cin>>roll_num;
+    cout<<"name";
+    cin>>name;
+    cout<<"dept";
+    cin>>dept;
+    cout<<"roll_num"<<roll_num<<"name"<<name<<"dept"<<dept;
+ }
