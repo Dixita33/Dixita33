bf70f6d75b9d6f48b9a8d37c2733cc615229aa28
Dixita33 <168657070+Dixita33@users.noreply.github.com>
2024-05-05 21:21:06 +0530

    Create Pract4.cpp

---
+++ Pract4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  int a=10,b=6;
+if(a>b) 
+cout<<"\na is greater ";
+else 
+cout<<" \nb is greater ";
+if(a>=b) 
+cout<<"\na is greater or equal to b";
+else 
+cout<<" \nb is greater or equal to a";
+if(a!=b) 
+cout<<"\na is not equal to b";
+else 
+cout<<"\nboth are equal";
+return 0;
+}
